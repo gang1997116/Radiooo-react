1c5983c0f766091a167e8d4e0f001fe68cfc4dfa
Kelly Zhong <gang1997116@users.noreply.github.com>
2020-07-01 14:51:10 +0100

    fix the unmute debug

--- src/components/playControl/playControl.jsx
+++ src/components/playControl/playControl.jsx
@@ -7,16 +7,16 @@ import last from "../../img/last.svg";
 import sound from "../../img/sound.svg";
 import list from "../../img/list.svg";
 import soundMute from "../../img/sound-mute.svg";
-import Like from '../like';
-import SoundControl from './soundcontrol';
+import Like from "../like";
+import SoundControl from "./soundcontrol";
 import { Link } from "react-router-dom";
 
 function PlayControl({ id }) {
-  const [data,setData] = useState([]);
-  const [like,setLike]=useState(false);
-  const [mute,setMute]=useState(false);
-  const [value,setValue]=useState(30);
-  const [soundStyle,setSound]=useState(sound);
+  const [data, setData] = useState([]);
+  const [like, setLike] = useState(false);
+  const [mute, setMute] = useState(false);
+  const [value, setValue] = useState(30);
+  const [soundStyle, setSound] = useState(sound);
   useEffect(() => {
     async function getdata() {
       const { data } = await getRadio(id);
@@ -25,54 +25,53 @@ function PlayControl({ id }) {
     getdata();
   }, [id]);
   const imgUrl = "https://www.radioair.info/images_radios/";
-  const handleLike=()=>{
+  const handleLike = () => {
     setLike(!like);
-  }
-  const handleMute=()=>{
-    if(soundStyle===sound){
+  };
+  const handleMute = () => {
+    const audio = document.getElementById(`audio${id}`);
+    if (soundStyle === sound) {
       setSound(soundMute);
-      const audio = document.getElementById(`audio${id}`);
       audio.volume = 0;
-    }
-    else{
+    } else {
       setSound(sound);
+      audio.volume = value / 100;
     }
     setMute(!mute);
-  }
+  };
   const handleChange = (event, newValue) => {
-
     setValue(newValue);
     const audio = document.getElementById(`audio${id}`);
     audio.volume = newValue / 100;
-    if(newValue===0){
+    if (newValue === 0) {
       setSound(soundMute);
-    }
-    else{
+    } else {
       setSound(sound);
     }
   };
-    return (
-      <div className="bar">
-        <Link to="/shop">
+  return (
+    <div className="bar">
+      <Link to="/shop">
         <div className="radio-logo">
-          
           <img src={imgUrl + data.l} alt="" />
-        </div></Link>
-      <div className="radio-title">{data.n}</div>
-        <div className="controls">
-          <div className="like" ><Like onClick={handleLike} likes={like}/></div>
-          <img src={last} className="last" alt=""/>
-          <div className="play-button-circle">
-          <PlayButton id={id} src={data.u} />
-          </div>
-          <img src={next} className="next" alt=""/>
-          <img src={soundStyle} className="sound" onClick={handleMute} alt=""/>
         </div>
-        <SoundControl mute={mute} value={value} onChange={handleChange}/>
-        <img src={list} className="list" alt=""/>
+      </Link>
+      <div className="radio-title">{data.n}</div>
+      <div className="controls">
+        <div className="like">
+          <Like onClick={handleLike} likes={like} />
+        </div>
+        <img src={last} className="last" alt="" />
+        <div className="play-button-circle">
+          <PlayButton id={id} src={data.u} />
+        </div>
+        <img src={next} className="next" alt="" />
+        <img src={soundStyle} className="sound" onClick={handleMute} alt="" />
       </div>
-    );
-  
+      <SoundControl mute={mute} value={value} onChange={handleChange} />
+      <img src={list} className="list" alt="" />
+    </div>
+  );
 }
 
 export default PlayControl;
